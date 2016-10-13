9f5cfac292857da988d562749e23a057c763bbd5
jStaff <staffjacob@live.com>
2016-10-12 21:55:22 -0500

    done finally

--- js/comp.js
+++ js/comp.js
@@ -30,8 +30,6 @@ $('#spidergraphcomparison').spidergraph('addlayer', {
 });
 
 
-
-
 $('#product1').spidergraph({
         'fields': ['RAM','CPU','Low Weight','Good Reviews','Lower Price', 'Battery Life'],
         'increments': 5,
@@ -84,3 +82,42 @@ $('#product1').spidergraph({
     'data': [3, 2, 1, 4, 1, 5],
     'linear': true
     });	
+
+var globalDOMLaptops = 0;
+var showLaptops = [];
+var digitLaptops = [];
+var laptops = JSON.parse(localStorage.getItem("laptops"));
+if(laptops == null){
+    laptops = makeLaptopArray(productList);
+}  
+localStorage.setItem('digitLaptops', JSON.stringify(digitLaptops));
+localStorage.setItem('showLaptops', JSON.stringify(showLaptops));
+var i = 0;
+function addLaptopToDOM(laptop){
+    i += 1;
+    console.log(laptop);
+    var element = $('#compare'); //<div class="close" id="'+i+'close"> <div class="dig-it-button" id="btn'+i+'">I can <strong>Buy</strong> it</div><div class="break" id="'+i+'break>
+    var html = $('<div class="comp-product"><canvas id="canvas" style="background-image: url(' +laptop.image_addr +'); width: 400px; height: 300px; background-size: cover"></canvas><div style="background-color: rgba(0,0,0,0.2); height: 300px; width: 400px;"><div id="comp-spidergraphcontainer"><p>'+ laptop.title +'</p><p>'+laptop.price +'</p><p>'+ laptop.rating +'</p></div></div></div></div></div>');
+    element.append(html);
+
+
+    $('#btn'+globalDOMLaptops).live('click', function(e) { 
+        var num = parseInt(/\d+/.exec(e.srcElement.id));
+         $('#'+num+'comp-product').remove();
+         $('#'+num+'close').remove();
+         $('#'+num+'break').remove();
+         $('#btn'+num).remove();
+         console.log(showLaptops[0]);
+        digitLaptops.push(showLaptops[num]);
+        localStorage.setItem('digitLaptops', JSON.stringify(digitLaptops));
+        console.log("Digged it!");
+    });
+}
+function addAllDigged(){
+    var diggedLaptops = JSON.parse(localStorage.getItem('digitLaptops'));
+    for(var i = 0; i < 3; i++){
+        addLaptopToDOM(diggedLaptops[i]);
+    }
+}
+window.onload = addAllDigged;
+
